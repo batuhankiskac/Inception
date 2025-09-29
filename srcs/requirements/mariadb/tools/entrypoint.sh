4777284e92b0351a1763609eeb97c3198d9f4c00
batuhankiskac <kiskacbatuhan@gmail.com>
2025-09-29 20:20:45 +0300

    Update Docker configurations: fix volume mappings, correct typos, and add Nginx entrypoint script for SSL certificate generation

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -11,21 +11,21 @@ chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "[mariadb] initializing data directory..."
-	mariadb-install-db --user=mysql --data-dir=/var/lib/mysql > /dev/null
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
 	echo "[mariadb] applying bootstrap SQL..."
 	tmp_sql="$(mktemp)"
 
-	cat > "$tmp_sql" << SQL
-	FLUSH PRIVILEGES;
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-	CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-	FLUSH PRIVILEGES;
-	SQL
+	cat > "$tmp_sql" <<SQL
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+SQL
 
-	mysqld --user=mysql --data-dir=/var/lib/mysql --bootstrap < "$tmp_sql"
+	mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap < "$tmp_sql"
 	rm -f "$tmp_sql"
 fi
 
