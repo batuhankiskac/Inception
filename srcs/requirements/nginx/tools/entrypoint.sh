e613e1529d790b8ba93dd1d34567924d564725f8
batuhankiskac <kiskacbatuhan@gmail.com>
2026-02-02 17:09:10 +0300

    Add Nginx configuration and entrypoint script for SSL setup

---
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [! -f /etc/nginx/ssl/inception.crt]; then
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	 -keyout /etc/nginx/ssl/inception.key \
+	 -out /etc/nginx/ssl/inception.crt \
+	 -subj "/C=TR/ST=ISTANBUL/L=ISTANBUL/O=42ISTANBUL/OU=42/CN=$DOMAIN_NAME"
+fi
+
+exec nginx -g 'daemon off;'
