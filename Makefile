a26341a63f0986863d76220f09b86ea67cd892b3
batuhankiskac <kiskacbatuhan@gmail.com>
2026-02-04 14:53:55 +0300

    Add Docker and configuration files for MariaDB and WordPress setup

---
+++ Makefile
@@ -0,0 +1,18 @@
+COMPOSE = docker-compose -f ./srcs/docker-compose.yml
+DATA_PATH = /home/bkiskac/data
+
+all:
+	mkdir -p $(DATA_PATH)/mariadb
+	mkdir -p $(DATA_PATH)/wordpress
+	$(COMPOSE) up -d --build
+
+down:
+	$(COMPOSE) down
+
+fclean: down
+	docker system prune -af --volumes
+	sudo rm -rf $(DATA_PATH)
+
+re: fclean all
+
+.PHONY: all down fclean re
---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [! -f /var/www/wordpress/wp-config.php]; then
+	wp core download --allow-root
+
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
+		--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author	 --allow-root
+fi
+
+exec /usr/sbin/php-fpm7.4 -F
