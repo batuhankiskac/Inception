c7fd7d666a1a1819a4c490508158ba8ec29677ac
batuhankiskac <kiskacbatuhan@gmail.com>
2026-02-04 22:23:03 +0300

    Fix variable names in WordPress entrypoint script for database configuration

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -3,8 +3,8 @@
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	wp core download --allow-root
 
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER \
-		--dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+	wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD --dbhost=mariadb --allow-root
 
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
 		--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
